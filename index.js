02d4ca5f5e5615f945c6ec5ac42a20c010b712cf
chad kidd <ckiddwebdev@gmail.com>
2019-05-07 14:28:12 -0700

    all api calls working with cookies functionality

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const bcrypt = require('bcryptjs'); /// <<<<<<<<<<<<<<<<<<<< yarn add bcryptjs
+
 const session = require('express-session');
 
 const db = require('./database/dbConfig.js');
@@ -27,15 +28,28 @@ const sessionConfig = {
   saveUninitialized: true,
 }
 
+//restricted-middleware
+// const restrictedMiddleware = (req, res, next) => {
+// if(req.session && req.session.username) {
+//   next()
+// } else {
+//   res.status(401).json({message: 'Please login to access'})
+// }
+// }
+
+
 server.use(session(sessionConfig));
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+//GET working
 server.get('/', (req, res) => {
   res.send("It's alive!");
 });
 
+
+//POST - register working
 server.post('/api/register', (req, res) => {
   let user = req.body;
   // check for username and password
@@ -53,18 +67,27 @@ server.post('/api/register', (req, res) => {
     });
 });
 
-server.post('/api/login', (req, res) => {
+
+//POST - login working with valid credentials logic written and working 
+//if invalid credentials provided
+//login working but not with restrictedMiddleware ???? gives 401 error
+server.post('/api/login',  (req, res) => {
   let { username, password } = req.body;
 
   // we compare the password guess against the database hash
   Users.findBy({ username })
     .first()
     .then(user => {
-      //
       if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({ message: `Welcome ${user.username}!` });
+        req.session.username = user.username;
+        //session username store
+        res.status(200).json(res.send( 
+          //welcome login message
+          `Welcome ${user.username}! have a cookie`));
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401);
+        //invalid credentials message when logging in with wrong credentials 
+        res.send('INVALID CREDENTIALS: You Shall NOT Pass!!')
       }
     })
     .catch(error => {
@@ -72,7 +95,9 @@ server.post('/api/login', (req, res) => {
     });
 });
 
+
 // protect this route, users must provide valid username/password to see the list of users
+//get working showing array of all users
 server.get('/api/users', protected, (req, res) => {
   Users.find()
     .then(users => {
@@ -80,6 +105,16 @@ server.get('/api/users', protected, (req, res) => {
     })
     .catch(err => res.send(err));
 });
+
+server.get('/api/logout', (req, res) => {
+  //if logged in
+  if(req.session) {
+  req.session.destroy();
+  } 
+  else {
+  res.end('already logged out')
+  }
+})
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
